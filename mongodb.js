72569aa0b31f480f4cdbdfc0557ccf0fcd544e04
mitul-ch <raselqax@gmail.com>
2022-10-30 21:17:52 +0600

    index

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,8 @@
 // CRUD System
-
 const { MongoClient, ObjectId } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
-
 MongoClient.connect(
   connectionURL,
   { useNewUrlParser: true },
